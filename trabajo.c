5027e7dc812d4f2f053fde4e12d9c3f07d6571f3
Marcos <marcos.rdelgado@alumnos.upm.es>
2023-04-19 22:25:13 +0200

    Update trabajo.c

--- trabajo.c
+++ trabajo.c
@@ -2,4 +2,12 @@
 #include<stdlib.h>
 
 // un menu que permita al usuario elegir la tarea a realizar
-//
+// vectores de estructuras
+// ficheros para leer y escribir
+// prohibido el uso de variables fuera de main
+// estructurar codigo con funciones
+// uso de cabeceras de c
+// obligatorio uso de github
+// documentacion en una wiki de github
+// diagrama de flujo que explique el proyecto
+
