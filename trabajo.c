6f52af8fa3b2343e090fa677f7294113723d9bcb
Marcos <marcos.rdelgado@alumnos.upm.es>
2023-04-24 17:12:52 +0200

    Lectura del fichero
    en vez de imprimirlo por el terminnal habria que guardar los datos en una estructura

--- trabajo.c
+++ trabajo.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
+
+#define LONGUITUD_MAXIMA 1024
+
+int lectura(int arcg, char **argv);
 // un menu que permita al usuario elegir la tarea a realizar
 // vectores de estructuras
 // ficheros para leer y escribir
@@ -11,3 +16,27 @@
 // documentacion en una wiki de github
 // diagrama de flujo que explique el proyecto
 
+int lectura(int arcg,  char **argv)
+{
+    FILE *fp;
+    char linea[LONGUITUD_MAXIMA];
+    char *palabra;
+
+    fp = fopen("generacion_por_tecnologias_21_22.csv","r");
+    if(fp == NULL)
+    {
+        printf("error al abrir el archivo\n");
+        return -1;
+    }
+    while(fgets(linea, LONGUITUD_MAXIMA, fp) != NULL)
+    {
+        palabra = strtok(linea,",");
+        while(palabra != NULL)
+        {
+            printf("%s\n", palabra); //esto habria que guardarlo en una estructura en vez de escribirlo
+            palabra = strtok(NULL,",");
+        }
+    }
+    fclose(fp);
+    return 0;
+}
